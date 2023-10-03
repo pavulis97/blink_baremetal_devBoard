d1040be75e784105c0f2539a6afea9ffcbdda15d
pavulis97 <pavulis97@gmail.com>
2023-10-03 14:32:34 +0300

    Revert "First build"
    This reverts commit e56740064ac0a0355a06a95d30d03dcf27adfbbf.

--- vscode-build/obj/app.d
+++
@@ -1,5 +0,0 @@
-vscode-build/obj/app.o: app.c \
- gecko_sdk_4.3.1/platform/common/toolchain/inc/sl_gcc_preinclude.h \
- blink.h
-gecko_sdk_4.3.1/platform/common/toolchain/inc/sl_gcc_preinclude.h:
-blink.h:
