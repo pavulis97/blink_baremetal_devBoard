e56740064ac0a0355a06a95d30d03dcf27adfbbf
pavulis97 <pavulis97@gmail.com>
2023-10-03 13:22:50 +0300

    First build

---
+++ vscode-build/obj/main.d
@@ -0,0 +1,76 @@
+vscode-build/obj/main.o: main.c \
+ gecko_sdk_4.3.1/platform/common/toolchain/inc/sl_gcc_preinclude.h \
+ autogen/sl_component_catalog.h \
+ gecko_sdk_4.3.1/platform/service/system/inc/sl_system_init.h app.h \
+ gecko_sdk_4.3.1/platform/service/power_manager/inc/sl_power_manager.h \
+ config/sl_power_manager_config.h \
+ gecko_sdk_4.3.1/platform/common/inc/sl_slist.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/stdlib.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/machine/ieeefp.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/_ansi.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/newlib-nano/newlib.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/_newlib_version.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/config.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/features.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/reent.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/_ansi.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/_types.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/machine/_types.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/lock.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/assert.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/cdefs.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/machine/stdlib.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/stdint.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/_stdint.h \
+ gecko_sdk_4.3.1/platform/common/inc/sl_status.h \
+ gecko_sdk_4.3.1/platform/service/sleeptimer/inc/sl_sleeptimer.h \
+ gecko_sdk_4.3.1/platform/common/inc/sl_common.h \
+ gecko_sdk_4.3.1/platform/common/inc/sl_assert.h \
+ gecko_sdk_4.3.1/platform/CMSIS/Core/Include/cmsis_compiler.h \
+ gecko_sdk_4.3.1/platform/CMSIS/Core/Include/cmsis_gcc.h \
+ gecko_sdk_4.3.1/platform/common/inc/sl_enum.h \
+ gecko_sdk_4.3.1/platform/emlib/inc/em_core_generic.h \
+ gecko_sdk_4.3.1/platform/service/system/inc/sl_system_process_action.h
+gecko_sdk_4.3.1/platform/common/toolchain/inc/sl_gcc_preinclude.h:
+autogen/sl_component_catalog.h:
+gecko_sdk_4.3.1/platform/service/system/inc/sl_system_init.h:
+app.h:
+gecko_sdk_4.3.1/platform/service/power_manager/inc/sl_power_manager.h:
+config/sl_power_manager_config.h:
+gecko_sdk_4.3.1/platform/common/inc/sl_slist.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/stdlib.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/machine/ieeefp.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/_ansi.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/newlib-nano/newlib.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/_newlib_version.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/config.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/features.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/reent.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/_ansi.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/_types.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/machine/_types.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/machine/_default_types.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/lock.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/assert.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/cdefs.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/machine/stdlib.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/stdint.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/_intsup.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/_stdint.h:
+gecko_sdk_4.3.1/platform/common/inc/sl_status.h:
+gecko_sdk_4.3.1/platform/service/sleeptimer/inc/sl_sleeptimer.h:
+gecko_sdk_4.3.1/platform/common/inc/sl_common.h:
+gecko_sdk_4.3.1/platform/common/inc/sl_assert.h:
+gecko_sdk_4.3.1/platform/CMSIS/Core/Include/cmsis_compiler.h:
+gecko_sdk_4.3.1/platform/CMSIS/Core/Include/cmsis_gcc.h:
+gecko_sdk_4.3.1/platform/common/inc/sl_enum.h:
+gecko_sdk_4.3.1/platform/emlib/inc/em_core_generic.h:
+gecko_sdk_4.3.1/platform/service/system/inc/sl_system_process_action.h:
